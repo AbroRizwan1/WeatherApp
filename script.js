19655caa74eb5158c2c6a9000e7783aea31558d1
AbroRizwan1 <Rizwanabro14@gmail>
2024-04-14 17:06:28 +0500

    first commit

--- script.js
+++ script.js
@@ -9,41 +9,66 @@ const searchbtn = document.querySelector(".button");
 const icon = document.querySelector("#icon");
 const weatherName = document.querySelector(".weather");
 
+
+
+
 searchbtn.addEventListener("click", function () {
   checkWeather(searchBox.value);
 });
 
+
+// Add event listener for Enter key press
+searchBox.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    // Check if Enter key is pressed
+    checkWeather(searchBox.value);
+  }
+});
+
+
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
   var data = await response.json();
+
+
+  if (!response.ok) {
+    // If response is not okay (e.g., 404 error), show error message
+    document.querySelector(".city").innerHTML = "City not found";
+    return; 
+  }
+
 
   console.log(data);
 
-
-  document.querySelector(".city").innerHTML = data.name;
+  var city =  document.querySelector(".city")
+  city.innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "℃";
   document.querySelector(".humidity").innerHTML = data.main.humidity;
   document.querySelector(".wind").innerHTML = data.wind.speed;
 
-  
-
-if (data.weather[0].main === "Clear") {
-  icon.className = "fas fa-sun"; 
-  weatherName.textContent = "Clear";
-} else if (data.weather[0].main === "Drizzle") {
-  icon.className = "fas fa-cloud-showers-heavy"; 
-  weatherName.textContent = "Drizzle";
-} else if (data.weather[0].main === "Rain") {
-  icon.className = "fas fa-cloud-rain"; 
-  weatherName.textContent = "Rain";
-}
 
 
 
-const currentDate = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-document.querySelector(".dateData").innerHTML = currentDate.toLocaleDateString('en-US', options);
-document.querySelector(".timeData").innerHTML = currentDate.toLocaleTimeString('en-US');
+  if (data.weather[0].main === "Clear") {
+    icon.className = "fas fa-sun";
+    weatherName.textContent = "Clear";
+  } else if (data.weather[0].main === "Drizzle") {
+    icon.className = "fas fa-cloud-showers-heavy";
+    weatherName.textContent = "Drizzle";
+  } else if (data.weather[0].main === "Rain") {
+    icon.className = "fas fa-cloud-rain";
+    weatherName.textContent = "Rain";
+  }
 
+  const currentDate = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  document.querySelector(".dateData").innerHTML =
+    currentDate.toLocaleDateString("en-US", options);
+  document.querySelector(".timeData").innerHTML =
+    currentDate.toLocaleTimeString("en-US");
 }
